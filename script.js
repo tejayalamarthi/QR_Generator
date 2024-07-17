81e9ad7906d37e3b2252b77ad41dd79c20ef91d8
YA40084653 <YA40084653@L-158001097>
2024-07-17 23:02:10 +0530

    main

---
+++ script.js
@@ -0,0 +1,17 @@
+let qrurl = "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=";
+let imgbox = document.getElementById("imgBox");
+let qrimage = document.getElementById("qrimage"); 
+let qrtext = document.getElementById("qrtext");
+
+function generatQR(){
+    if(qrtext.value.length> 0){
+        qrimage.src = "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=" + qrtext.value;
+        imgbox.classList.add("show-img");
+    }else{
+        qrtext.classList.add('error');
+        setTimeout(()=>{
+            qrtext.classList.remove("error");
+        },1000)
+    }
+
+ }
